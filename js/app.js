a07e355f0383e2757189cae751dd2cb61cb8e3e3
36092694 <36092694@qq.com>
2017-10-06 11:07:36 +0800

    dd

--- js/app.js
+++ js/app.js
@@ -3,6 +3,105 @@
  * 当您要参考这个演示程序进行相关 app 的开发时，
  * 请注意将相关方法调整成 “基于服务端Service” 的实现。
  **/
+//全局常量
+var APP_SERVER_WWW_URL = 'http://www.15scsc.com';
+var APP_SERVER_URL = 'http://m.15scsc.com';
+var APP_LOGIN_URL = '/APP/login.ashx';
+var APP_REG_URL = '/APP/reg.ashx';
+var APP_Carousel_URL = '/APP/JSONdata/Carousel.json';
+var APP_NewList_URL = '/APP/GetNewList.ashx';
+var APP_NewContent_URL = '/APP/Getnewcotent.ashx';
+var APP_Productlist_URL = '/APP/GetProduct_List.ashx';
+var APP_GetPro_List_app= '/APP/Product/GetPro_List_app.ashx';//首页产品展示
+var APP_Get_menu_dh= '/app/Product/Get_menu_dh.ashx'; //导航数据
+var APP_allgoods_query="/app/Product/GetPro_ByClassId.ashx";//获取所有商品
+var APP_allgoods_classfy ="/app/Product/ProClass.ashx";//获取所有分类
+var APP_getUser_info="/app/UserName/GetUser.ashx";//获取用户信息
+var APP_allgoods_order="/app/orders/Get_orders.ashx";//获取用户订单
+var APP_user_address="/app/UserName/Get_address.ashx";//获取用户地址
+var APP_user_addressSet="/app/UserName/Set_address.ashx"//设置默认地址
+var APP_user_delAddress="/app/UserName/del_address.ashx"//删除地址
+var APP_user_infoChange="/app/UserName/UpdateUser.ashx"//修改个人信息
+var APP_user_passChange="/app/UserName/UpdatePass.ashx"//修改密码
+var APP_goodDetail_getImg="/app/Product/Getdetails_pic.ashx"//获取详情图片
+var User_Username = ""; ////账号
+var User_name = ""; //姓名
+var UserId = ""; //ID
+var UserLevel = "E"; //等级
+var Islogin = 0; //是否登录
+function U_getState() {
+	var stateText = localStorage.getItem('$state') || "{}";
+	return JSON.parse(stateText);
+};
+//验证是否登录
+function U_islogin() {
+	var stateText = localStorage.getItem('$state') || "{}";
+	if(stateText != "{}") {
+		var UserState = JSON.parse(stateText);
+		if(UserState.account != undefined) {
+			User_Username = UserState.User_Username; //账号
+			User_name = UserState.User_name; //姓名
+			UserId = UserState.UserId; //ID
+			UserLevel = UserState.UserLevel; //等级
+			Islogin = 1; //是否登录
+		}
+	} else {
+		Islogin = 0;
+	}
+}
+//会员中心验证是否登录有登跳转至登录
+function U_islogin_url(val) {
+	
+	
+	var stateText = localStorage.getItem('$state') || "{}";
+	if(stateText != "{}") {
+		var UserState = JSON.parse(stateText);
+		if(UserState.account != undefined) {
+			User_Username = UserState.User_Username; //账号
+			User_name = UserState.User_name; //姓名
+			UserId = UserState.UserId; //ID
+			UserLevel = UserState.UserLevel; //等级
+		
+		}
+	} else {
+		if (val==2)
+		{
+			window.location="../../login.html";
+		}
+		else if (val==1)
+		{
+				window.location="../login.html";
+		}
+		else
+		{
+				window.location="login.html";
+		}
+			
+	}
+}
+
+
+
+function OPENrulid(url, val) {
+	mui.openWindow({
+		url: url,
+		id: url,
+		title: "正在加载",
+		show: {
+			autoShow: "true",
+			aniShow: "slide-in-right",
+		},
+		waiting: {
+			autoShow: true, //自动显示等待框，默认为true
+			title: '正在加载...', //等待对话框上显示的提示内容
+		},
+		extras: {
+			val: val
+
+		}
+	});
+}
+
 (function($, owner) {
 	/**
 	 * 用户登录
@@ -12,27 +111,52 @@
 		loginInfo = loginInfo || {};
 		loginInfo.account = loginInfo.account || '';
 		loginInfo.password = loginInfo.password || '';
-		if (loginInfo.account.length < 5) {
+		if(loginInfo.account.length < 5) {
+
 			return callback('账号最短为 5 个字符');
 		}
-		if (loginInfo.password.length < 6) {
+		if(loginInfo.password.length < 6) {
 			return callback('密码最短为 6 个字符');
 		}
-		var users = JSON.parse(localStorage.getItem('$users') || '[]');
-		var authed = users.some(function(user) {
-			return loginInfo.account == user.account && loginInfo.password == user.password;
-		});
-		if (authed) {
-			return owner.createState(loginInfo.account, callback);
-		} else {
-			return callback('用户名或密码错误');
-		}
-	};
+		var mask = mui.createMask(); //遮罩层
 
-	owner.createState = function(name, callback) {
+		mui.ajax(APP_SERVER_URL + APP_LOGIN_URL, {
+			data: {
+				username: loginInfo.account,
+				password: loginInfo.password
+			},
+			dataType: 'json', //服务器返回json格式数据
+			type: 'post', //HTTP请求类型
+			timeout: 10000, //超时时间设置为10秒；
+			beforeSend: function() {
+				mask.show(); //显示遮罩层
+			},
+			complete: function() {
+				mask.close(); //关闭遮罩层
+			},
+			success: function(data) {
+				if(data.state == '1') {
+					console.log(9999)
+					return owner.createState(data.username, data.name, data.UserLevel, data.UserId, callback);
+				} else {
+					return callback(data.error);
+				}
+			},
+			error: function(xhr, type, errorThrown) {
+				//异常处理；
+				console.log(errorThrown);
+				callback('服务器连接超时');
+			}
+		});
+
+	}
+
+	owner.createState = function(username, name, UserLevel, userid, callback) {
 		var state = owner.getState();
-		state.account = name;
-		state.token = "token123456789";
+		state.Username = username;
+		state.name = name;
+		state.UserLevel = UserLevel;
+		state.UserId = userid;
 		owner.setState(state);
 		return callback();
 	};
@@ -43,21 +167,51 @@
 	owner.reg = function(regInfo, callback) {
 		callback = callback || $.noop;
 		regInfo = regInfo || {};
-		regInfo.account = regInfo.account || '';
+		regInfo.phoneNum = regInfo.phoneNum || '';
 		regInfo.password = regInfo.password || '';
-		if (regInfo.account.length < 5) {
+		if(regInfo.phoneNum.length < 5) {
 			return callback('用户名最短需要 5 个字符');
 		}
-		if (regInfo.password.length < 6) {
+		if(regInfo.password.length < 6) {
 			return callback('密码最短需要 6 个字符');
 		}
-		if (!checkEmail(regInfo.email)) {
-			return callback('邮箱地址不合法');
-		}
-		var users = JSON.parse(localStorage.getItem('$users') || '[]');
-		users.push(regInfo);
-		localStorage.setItem('$users', JSON.stringify(users));
-		return callback();
+
+		//注册保存
+
+		var mask = mui.createMask(); //遮罩层
+		console.log(APP_SERVER_URL + APP_REG_URL);
+		mui.ajax(APP_SERVER_URL + APP_REG_URL, {
+			data: {
+				username: regInfo.phoneNum,
+				password: regInfo.password,
+				name: regInfo.name
+			},
+			dataType: 'json', //服务器返回json格式数据
+			type: 'post', //HTTP请求类型
+			timeout: 10000, //超时时间设置为10秒；
+			beforeSend: function() {
+
+				mask.show(); //显示遮罩层
+			},
+			complete: function() {
+
+				mask.close(); //关闭遮罩层
+			},
+			success: function(data) {
+
+				//mui.alert(data.state);
+				if(data.state == '1') {
+					return owner.createState(data.username, data.name, data.UserLevel, callback);
+				} else {
+					return callback(data.error);
+				}
+			},
+			error: function(xhr, type, errorThrown) {
+				//异常处理；
+				callback('服务器连接超时');
+			}
+		});
+
 	};
 
 	/**
@@ -81,7 +235,7 @@
 
 	var checkEmail = function(email) {
 		email = email || '';
-		return (email.length > 3 && email.indexOf('@') > -1);
+		return(email.length > 3 && email.indexOf('@') > -1);
 	};
 
 	/**
@@ -89,7 +243,7 @@
 	 **/
 	owner.forgetPassword = function(email, callback) {
 		callback = callback || $.noop;
-		if (!checkEmail(email)) {
+		if(!checkEmail(email)) {
 			return callback('邮箱地址不合法');
 		}
 		return callback(null, '新的随机密码已经发送到您的邮箱，请查收邮件。');
@@ -107,30 +261,29 @@
 	 * 设置应用本地配置
 	 **/
 	owner.getSettings = function() {
-			var settingsText = localStorage.getItem('$settings') || "{}";
-			return JSON.parse(settingsText);
-		}
-		/**
-		 * 获取本地是否安装客户端
-		 **/
+		var settingsText = localStorage.getItem('$settings') || "{}";
+		return JSON.parse(settingsText);
+	}
+	/**
+	 * 获取本地是否安装客户端
+	 **/
 	owner.isInstalled = function(id) {
-		if (id === 'qihoo' && mui.os.plus) {
+		if(id === 'qihoo' && mui.os.plus) {
 			return true;
 		}
-		if (mui.os.android) {
+		if(mui.os.android) {
 			var main = plus.android.runtimeMainActivity();
 			var packageManager = main.getPackageManager();
 			var PackageManager = plus.android.importClass(packageManager)
 			var packageName = {
 				"qq": "com.tencent.mobileqq",
-				"weixin": "com.tencent.mm",
-				"sinaweibo": "com.sina.weibo"
+				"weixin": "com.tencent.mm"
 			}
 			try {
 				return packageManager.getPackageInfo(packageName[id], PackageManager.GET_ACTIVITIES);
-			} catch (e) {}
+			} catch(e) {}
 		} else {
-			switch (id) {
+			switch(id) {
 				case "qq":
 					var TencentOAuth = plus.ios.import("TencentOAuth");
 					return TencentOAuth.iphoneQQInstalled();
--- js/main.js
+++ js/main.js
@@ -1,104 +1,49 @@
-var jq = $.noConflict();
 mui.ready(function() {
 	/***********************************************************banner***********************************************************/
 	var gallery = mui("#slider");
 	gallery.slider({
 		interval: 5000
 	});
-	/************************************************************跑马灯*************************************************************/
-	var container = document.getElementById("container");
-	var inContainer = document.getElementById("inContainer");
-	var st;
-	// 移动,停下。
-	function move() {
-		if(container.scrollLeft < container.scrollWidth / 2) {
-			container.scrollLeft++;
-			st = setTimeout(move, 15);
-		}
-		if(container.scrollLeft == container.scrollWidth / 2) {
-			clearTimeout(st);
-			container.scrollLeft = 0;
-			setTimeout(move, 1000);
-		}
-	}
-	move();
-
-})
-jq(document).ready(function() {
-	/******************************************************点击显示隐藏******************************************************************/
-	jq(".quick-buy").click(function() {
-		jq(".select-hide").slideDown()
-	})
-	jq(".select-hide-main .goods-option").click(function() {
-		jq(this).removeClass("option-notactive").addClass("option-active").siblings().removeClass("option-active").addClass("option-notactive")
-	})
-	jq(".select-hide-bottom").click(function() {
-		jq(".select-hide").slideUp()
+});
+$(document).ready(function() {
+	/************************************************商品排序点击切换*******************************************************/
+	$("#allgoods-logo>a").click(function  () {
+		$(this).find("div").addClass(".sort-active")
+		$(this).siblings().find("div").removeClass(".sort-active")
 	})
 	/**********************************删除收藏*****************************************/
-	jq("#collect .iconfont").click(function() {
-		var index = jq("#collect .iconfont").index(jq(this))
-		jq("#collect>li").eq(index).remove()
+	$("#collect .iconfont").click(function() {
+		var index = $("#collect .iconfont").index($(this));
+		$("#collect>li").eq(index).remove();
 	})
 	/****************************************取消订单***************************************/
-	jq(".mui-slider-group .indent-cancel").click(function() {
-		var index = jq(".mui-slider-group .indent-cancel").index(jq(this))
-		jq(".mui-slider-group ul").eq(index).remove()
+	$(".mui-slider-group .indent-cancel").click(function() {
+		var index = $(".mui-slider-group .indent-cancel").index($(this))
+		$(".mui-slider-group ul").eq(index).remove()
+		var sum1 = $("#item1mobile>.mui-table-view").size()
+		$("#sliderSegmentedControl>a").eq(0).children(".mui-badge ").text(sum1)
+		var sum2 = $("#item2mobile>.mui-table-view").size()
+		$("#sliderSegmentedControl>a").eq(1).children(".mui-badge ").text(sum2)
+		var sum3 = $("#item3mobile>.mui-table-view").size()
+		$("#sliderSegmentedControl>a").eq(2).children(".mui-badge ").text(sum3)
+		var sum4 = $("#item4mobile>.mui-table-view").size()
+		$("#sliderSegmentedControl>a").eq(3).children(".mui-badge ").text(sum4)
 	})
-	/****************************************购物车删除商品****************************************/
-	jq("#shopping-cart-goods .goods-delete").click(function() {
-		var index = jq("#shoppingcart-goods-cell .good-delete").index(jq(this))
-		jq("#shopping-cart-goods .shoppingcart-goods-cell").eq(index).remove()
-		console.log(index)
+	
+	/********************************选择支付方式***********************************/
+	$("#afford-select .mui-collapse-content").click(function() {
+		var index = $("#afford-select .mui-collapse-content").index($(this))
+		$("#afford-select .iconfont").eq(index).removeClass("hide").addClass("show")
+		$(this).siblings().children(".iconfont").removeClass("show").addClass("hide")
+		var sel = $(this).children(".mui-pull-left").text()
+		$(".payment-pattern span").eq(1).text(sel)
+
 	})
-	//删除所有商品
-	jq(".empty-cart").click(function() {
-		jq("#shopping-cart-goods .shoppingcart-goods-cell").each(function() {
-			jq(this).remove()
-		})
+	/********************************选择优惠券***********************************/
+	$("#coupon-select .mui-collapse-content").click(function() {
+		var index = $("#coupon-select .mui-collapse-content").index($(this))
+		var sel = $(this).children(".mui-pull-left").text()
+		$(".mui-navigate-right").eq(0).text(sel)
+		$("#coupon-select").removeClass("mui-active")
 	})
-	/*********************删除地址****************************/
-	jq(".address-cell .address-delete").click(function() {
-		var index = jq(".address-cell .address-delete").index(jq(this))
-		jq(".mui-content .address-cell").eq(index).remove()
-	})
-	/***********************清空浏览历史************************************/
-	jq(".empty-history").click(function() {
-		jq(".mui-content>a").remove()
-	})
-	/**********************************购物车飞入动画*******************************/
-	var offset = jq(".cart-details").offset();
-	jq(".cart-details").click(function(event) {
-		var addcar = jq(this);
-		var img = jq(".fisrtimg").attr('src');
-		var flyer = jq('<img class="u-flyer" src="' + img + '">');
-		goodsum = jq(".circle1").text();
-		goodsum++;
-		flyer.fly({
-			start: {
-				left: (screen.width) / 2, //开始位置（必填）#fly元素会被设置成position: fixed 
-				top: (screen.height) / 3 //开始位置（必填） 
-			},
-			end: {
-				left: offset.left + (screen.width) / 20, //结束位置（必填） 
-				top: offset.top + 10, //结束位置（必填） 
-				width: 0, //结束时宽度 
-				height: 0 //结束时高度 
-			},
-			onEnd: function() { //结束回调 
-				this.destory(); //移除dom 
-			}
-		});
-		jq(".circle1").text(goodsum)
-	});
-	//			});
-	/**************************判断购物车是否为空*******************************/
-	var goodsum=10
-	if(goodsum == 0) {
-		jq(".mui-content1").show()
-		jq(".mui-content2").hide()
-	} else {
-		jq(".mui-content1").hide()
-		jq(".mui-content2").show()
-	}
 })
